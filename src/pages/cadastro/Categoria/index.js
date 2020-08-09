df52269d453d48d202f0ae5961140613563c1e25
rabinatti <65193510+rabinatti@users.noreply.github.com>
2020-08-08 23:25:23 -0300

    Update index.js

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -7,7 +7,7 @@ function CadastroCategoria () {
         <PageDefault>
             <h1>Cadastro de Categoria</h1>
             
-            <Link to="/cadastro/categoria">
+            <Link to="/">
                Ir para home
             </Link>
         </PageDefault>
